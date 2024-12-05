7a1910e4873639372f4d9d9080f013bb83fb9487
MalditaS <sarafdzlor@gmail.com>
2024-12-05 10:22:26 +0100

    05/12/2024

--- Repaso_Profe/ej2.bat
+++ Repaso_Profe/ej2.bat
@@ -1,9 +1,16 @@
 @echo off
 title Mi PC en un vistazo
 
+echo Este es tu nombre de usuario:
 whoami
+
+echo Esta es la version de tu sistema operativo:
 ver
+
+echo Esta es la cantidad de memoria RAM instalada:
 wmic memphysical get MaxCapacity, MemoryDevices
+
+echo Este es tu directorio actual:
 cd
 
 pause>nul
